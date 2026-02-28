30354e08cd6652404cc3d1edf09485fdf2a0a0c5
Divya Bharti <sk7243407@gmail.com>
2026-02-28 21:54:45 +0530

    Practice component fix

--- basic-react-app/src/App.jsx
+++ basic-react-app/src/App.jsx
@@ -1,4 +1,6 @@
 // rafce
+import React from 'react'
+import Practice from './Practice.jsx'
 
 const App = () => {
   return (
@@ -6,9 +8,10 @@ const App = () => {
     <div id="dad">
       <h1 id="child1">Hello</h1>
       <h2 id="child2">Hello</h2>
+       <Practice />
     </div>
     <div id="chacha">
-
+        <Practice />
     </div>
     </>
   )
---
+++ basic-react-app/src/Practice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Practice = () => {
+  return (
+    <div>
+      <h1>Divya Bharti</h1>
+    </div>
+  )
+}
+
+export default Practice
